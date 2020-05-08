bc85bcfa4b0f14a3d4890cadd444b2ba5e09448b
NguyenHaIT2 <nguyenhath97@gmail.com>
2020-05-08 01:01:05 -0700

    ha

--- src/main/java/com/example/service/GiaovienService.java
+++ src/main/java/com/example/service/GiaovienService.java
@@ -1,11 +1,64 @@
 package com.example.service;
 
+import com.example.entity.GiaovienEntity;
+import com.example.entity.HocsinhEntity;
 import com.example.repository.GiaovienRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GiaovienService{
     @Autowired
     GiaovienRepository repository;
+    public List<GiaovienEntity> getGiaovienByTenLike(String ten) {
+        List<GiaovienEntity> giaovienlist = new ArrayList<GiaovienEntity>();
+        repository.findGiaovienEntitiesByTen(ten);
+        if (giaovienlist.size() > 0) {
+            return giaovienlist;
+        } else {
+            return new ArrayList<GiaovienEntity>();
+        }
+    }
+    public GiaovienEntity getGiaovienById(Long id) throws Exception {
+        Optional<GiaovienEntity> giaovien = repository.findById(id);
+
+        if (giaovien.isPresent()) {
+            return giaovien.get();
+        } else {
+            throw new Exception("No Congty record exist for given id");
+        }
+    }
+    public GiaovienEntity createOrUpdateGiaovien(GiaovienEntity entity) throws Exception {
+        Optional<GiaovienEntity> giaovien  = repository.findById(entity.getId());
+
+        if (giaovien.isPresent())
+        {
+            GiaovienEntity newEntity = giaovien.get();
+            newEntity.setTen(entity.getTen());
+            newEntity.setEmail(entity.getEmail());
+            newEntity.setMatkhau(entity.getMatkhau());
+            newEntity.setGroupid(entity.getGroupid());
+            newEntity = repository.save(newEntity);
+            return newEntity;
+        }
+        else
+        {
+            entity = repository.save(entity);
+            return entity;
+        }
+    }
+
+    public void deleteGiaovienById(Long id) throws Exception {
+        Optional<GiaovienEntity> giaovien  = repository.findById(id);
+
+        if (giaovien.isPresent()) {
+            repository.deleteById(id);
+        } else {
+            throw new Exception("No Congty record exist for given id");
+        }
+    }
 }
