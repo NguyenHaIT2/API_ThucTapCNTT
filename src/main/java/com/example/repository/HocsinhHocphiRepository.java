150f4d3430d04bbf5a48ca2d270fb4b5d9a42156
NguyenHaIT2 <nguyenhath97@gmail.com>
2020-05-09 02:26:05 -0700

    ha

--- src/main/java/com/example/repository/HocsinhHocphiRepository.java
+++ src/main/java/com/example/repository/HocsinhHocphiRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public
 interface HocsinhHocphiRepository extends JpaRepository<HocsinhHocphiEntity, Long> {
+    List<HocsinhHocphiEntity> findByMahocsinh(long mahocsinh);
+    List<HocsinhHocphiEntity> findByTinhtrang(int t);
 }
--- src/main/java/com/example/repository/PhuhuynhRepository.java
+++ src/main/java/com/example/repository/PhuhuynhRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public
 interface PhuhuynhRepository extends JpaRepository<PhuhuynhEntity, Long> {
+    List<PhuhuynhEntity> findPhuhuynhEntityByTen(String ten);
 }
