afb7b354ed9083a603734f5e36985cf42cd616fe
NguyenHaIT2 <nguyenhath97@gmail.com>
2020-05-19 01:52:58 -0700

    ha

--- src/main/java/com/example/repository/HocsinhVeRepository.java
+++ src/main/java/com/example/repository/HocsinhVeRepository.java
@@ -6,7 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public
 interface HocsinhVeRepository extends JpaRepository<HocsinhVeEntity, Long> {
+    List<HocsinhVeEntity> findByMahocsinh(long mahocsinh);
+    List<HocsinhVeEntity> findByNgay(Date ngay);
+    List<HocsinhVeEntity> findByMahocsinhAndNgay(long mahocsinh, Date ngay) ;
 }
