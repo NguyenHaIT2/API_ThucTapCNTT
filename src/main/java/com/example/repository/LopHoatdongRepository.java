6dda861ae6856f743fc5a1391de254ad279e0025
NguyenHaIT2 <nguyenhath97@gmail.com>
2020-05-19 06:21:33 -0700

    ha

--- src/main/java/com/example/repository/LopHoatdongRepository.java
+++ src/main/java/com/example/repository/LopHoatdongRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public
 interface LopHoatdongRepository extends JpaRepository<LopHoatdongEntity, LopHoatdongEntityPK> {
+    List<LopHoatdongEntity> findByMalop(long malop);
 }
