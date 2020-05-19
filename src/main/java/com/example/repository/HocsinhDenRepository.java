522efce6fa98d2b4bea8f70ca198356288c3027a
NguyenHaIT2 <nguyenhath97@gmail.com>
2020-05-19 01:41:59 -0700

    ha

--- src/main/java/com/example/repository/HocsinhDenRepository.java
+++ src/main/java/com/example/repository/HocsinhDenRepository.java
@@ -6,7 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public
 interface HocsinhDenRepository extends JpaRepository<HocsinhDenEntity, Long> {
+    List<HocsinhDenEntity> findByMahocsinh(long mahocsinh);
+    List<HocsinhDenEntity> findByNgay(Date ngay);
+    List<HocsinhDenEntity> findByMahocsinhAndNgay(long mahocsinh, Date ngay) ;
 }
--- src/main/java/com/example/service/DiemdanhdenService.java
+++ src/main/java/com/example/service/DiemdanhdenService.java
@@ -1,8 +1,13 @@
 package com.example.service;
 
+import com.example.entity.HocsinhEntity;
 import com.example.repository.DiemdanhdenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DiemdanhdenService{
