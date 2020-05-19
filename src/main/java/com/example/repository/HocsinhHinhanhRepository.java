1395f8335ba104188acce094514f77263a15b291
NguyenHaIT2 <nguyenhath97@gmail.com>
2020-05-19 06:04:51 -0700

    ha

--- src/main/java/com/example/repository/HocsinhHinhanhRepository.java
+++ src/main/java/com/example/repository/HocsinhHinhanhRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public
 interface HocsinhHinhanhRepository extends JpaRepository<HocsinhHinhanhEntity, HocsinhHinhanhEntityPK> {
+    List<HocsinhHinhanhEntity> findByMahinhanh(long mahinhanh);
+    List<HocsinhHinhanhEntity> findByMahocsinh(long mahocsinh);
 }
