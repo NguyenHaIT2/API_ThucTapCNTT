71c714f89b98ef11e9d655f403d43c6184f0888b
NguyenHaIT2 <nguyenhath97@gmail.com>
2020-05-19 06:24:36 -0700

    ha

--- src/main/java/com/example/repository/LopNoidunghoctapRepository.java
+++ src/main/java/com/example/repository/LopNoidunghoctapRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public
 interface LopNoidunghoctapRepository extends JpaRepository<LopNoidunghoctapEntity, LopNoidunghoctapEntityPK> {
+    List<LopNoidunghoctapEntity> findByMalop(long malop);
 }
--- src/main/java/com/example/service/LopNoidunghoctapService.java
+++ src/main/java/com/example/service/LopNoidunghoctapService.java
@@ -1,6 +1,8 @@
 package com.example.service;
 
 import com.example.entity.HocsinhEntity;
+import com.example.entity.LopHoatdongEntity;
+import com.example.entity.LopNoidunghoctapEntity;
 import com.example.entity.NoidunghoctapEntity;
 import com.example.repository.LopNoidunghoctapRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,5 +16,23 @@ import java.util.List;
 public class LopNoidunghoctapService{
     @Autowired
     LopNoidunghoctapRepository repository;
+    public List<LopNoidunghoctapEntity> getAllLopNoidunghoctap() {
+        List<LopNoidunghoctapEntity> list = new ArrayList<LopNoidunghoctapEntity>();
+        repository.findAll(Sort.by("manoidunghoctap").descending()).forEach(list::add);
 
+        if (list.size() > 0) {
+            return list;
+        } else {
+            return new ArrayList<LopNoidunghoctapEntity>();
+        }
+    }
+    public List<LopNoidunghoctapEntity> getLopNoidunghoctapByMalop(long malop ) throws Exception{
+        List<LopNoidunghoctapEntity> list = new ArrayList<LopNoidunghoctapEntity>();
+        repository.findByMalop(malop).forEach(list::add);
+        if (list.size() > 0) {
+            return list;
+        } else {
+            return new ArrayList<LopNoidunghoctapEntity>();
+        }
+    }
 }
